93ca8cef566b4b046dba3df160711b12217308e1
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2025-01-14 22:19:55 +0100

    set up reusable header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,37 +2,34 @@
     <footer class="bg-gray-800 text-white p-4 grid sm:grid-cols-2 grid-cols-1 gap-4">
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-items-center">
             <div>
-                1
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.
+               <h1>LOGO</h1>
+               <h2>Icons</h2>  
             </div>
 
             <div>
-                2
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.
+                <h1>Quick Links</h1>
+                <h2>About Us</h2>
+                <h2>About Us</h2>
+                <h2>About Us</h2>     
             </div>
         </div>
 
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-items-center">
             <div>
-                3
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.
+                <h1>Department and Ministries</h1>
+                <h2>About Us</h2>
+                <h2>About Us</h2>
+                <h2>About Us</h2>     
             </div>
 
             <div>
-                4
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.<br />
-                Lorem ipsum dolor sit amet.
+                <h1>Newsletter</h1>
+                <div class="bg-white rounded-md px-2">
+                    <input type="email" name="" placeholder="Your Email" id="" class="px-2 py-2 rounded-md focus:outline-none focus:ring-0 text-black">
+                    <span class="text-black px-1">ICON</span>
+                </div>                
+                <h2>Letter Icon - info@amez-ewaed.org</h2>
+                <h2>Phone Icon - +234 8036765479</h2>
             </div>
         </div>
         
