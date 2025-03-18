7ec114270ddd6937b17868da2bc53abcd4559afe
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2025-03-18 11:52:07 +0100

    adjusted nav bar

--- src/components/NavBarComponents/NavBarSm.vue
+++ src/components/NavBarComponents/NavBarSm.vue
@@ -7,10 +7,12 @@
         role="dialog"
         aria-expanded="sideBar"
       >
-      <div class="flex justify-between mb-10">
-        <div>Logo</div>
-        <button @click="setSideBar(false)" aria-label="Close sidebar">
-            <svg height="1rem" width="1.5rem" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve" fill="#000000">
+      <div class="flex justify-between mb-10  w-[100%]">
+        <div>                
+          <img src="/images/logo/amezlogo.png" alt="" srcset="" height="60" width="100">
+        </div>
+        <button @click="setSideBar(false)" aria-label="Close sidebar" class="w-16 flex justify-end item-center">
+            <svg height="3rem" width="2rem" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve" fill="#000000">
                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                 <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                 <g id="SVGRepo_iconCarrier"> <g> 
@@ -550,20 +552,22 @@
     </div>
 
     <div class="flex justify-between px-5 h-20 items-center border-b-2 border-gray-100 relative z-10"
-    :class="{ 'fixed-nav': isFixed, 'setNavBg': bgBlack  }"
+      :class="{ 'fixed-nav': isFixed, 'setNavBg': bgBlack  }">
 
-    >
-      <div>Logo</div>
-      <button @click="setSideBar(true)" aria-label="Open sidebar">
-        <svg height="4rem" width="2rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="#FFF">
-          <line x1="0" y1="21" x2="6" y2="21" stroke="#FFF" stroke-width="5"></line>
-          <line x1="16" y1="21" x2="64" y2="21" stroke="#FFF" stroke-width="5"></line>
-          <line x1="0" y1="33" x2="6" y2="33" stroke="#FFF" stroke-width="5"></line>
-          <line x1="16" y1="33" x2="64" y2="33" stroke="#FFF" stroke-width="5"></line>
-          <line x1="0" y1="45" x2="6" y2="45" stroke="#FFF" stroke-width="5"></line>
-          <line x1="16" y1="45" x2="64" y2="45" stroke="#FFF" stroke-width="5"></line>
-        </svg>
-      </button>
+        <div>                
+          <img src="/images/logo/amezlogo.png" alt="" srcset="" height="60" width="100">
+        </div>
+        <button @click="setSideBar(true)" aria-label="Open sidebar">
+          <svg height="4rem" width="2rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="#FFF">
+            <line x1="0" y1="21" x2="6" y2="21" stroke="#FFF" stroke-width="5"></line>
+            <line x1="16" y1="21" x2="64" y2="21" stroke="#FFF" stroke-width="5"></line>
+            <line x1="0" y1="33" x2="6" y2="33" stroke="#FFF" stroke-width="5"></line>
+            <line x1="16" y1="33" x2="64" y2="33" stroke="#FFF" stroke-width="5"></line>
+            <line x1="0" y1="45" x2="6" y2="45" stroke="#FFF" stroke-width="5"></line>
+            <line x1="16" y1="45" x2="64" y2="45" stroke="#FFF" stroke-width="5"></line>
+          </svg>
+        </button>
+
     </div>
 
     <slot></slot>
