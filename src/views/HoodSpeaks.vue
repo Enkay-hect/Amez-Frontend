bef4d3fbd4472ea56d80c85cd9b7fa922ee51150
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2025-04-04 15:58:50 +0100

    update to youth, lay and zion

--- src/views/HoodSpeaks.vue
+++ src/views/HoodSpeaks.vue
@@ -1,12 +1,14 @@
 <template>
-      <NavBar class="">
-        <div class="absolute inset-0 flex flex-col justify-center items-center gap-3 pt-24 bg-black w-[100%]">
+      <NavBar style="height: 12rem" class="bg-black">
+        
+    </NavBar>
+
+    <div class="relative inset-0 flex flex-col justify-center items-center gap-3 pt-20 bg-black w-[100%]">
             <div class="relative flex flex-col justify-center items-center w-full max-w-screen-lg pt-8">
                 <img src="/images/logo1.jpeg" alt="Logo" class="w-full h-auto max-h-[500px] object-contain">
                 <!-- <h2 class="absolute text-4xl font-extrabold text-white">THE BISHOP</h2> -->
             </div>
         </div>
-    </NavBar>
 
     <div class="p-4">
         <h2 class="text-2xl font-bold text-center mb-4">Hood-Speaks Gallery</h2>
