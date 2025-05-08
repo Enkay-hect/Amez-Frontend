c9ee8153c367205432abc911beeed09665b9b7b7
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2025-05-08 13:45:15 +0100

    update to BAA

--- src/views/TheBAA.vue
+++ src/views/TheBAA.vue
@@ -6,41 +6,84 @@
 
     <div class="relative w-full h-auto flex" >
         <!-- Main Content Area (Scrollable & Centered) -->
-        <div class=" sm:flex-1 h-[40rem] overflow-y-auto p-8 md:mr-56 justify-center scroll-smooth w-full mt-8" >
+        <div class=" sm:flex-1  p-8 sm:px-96 justify-center  w-full mt-8" >
 
-        <div class="flex flex-col justify-around items-center w-full text-center">
+        <div class="flex flex-row flex-wrap justify-around items-center w-full text-center">
 
-            <div class="flex flex-col md:flex-row gap-10 lg:gap-60 pb-8 justify-center items-center "  id="takis">
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center" id="EFREKEE">
                 <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
-                    <img src="/images/BAA/Dr-TAKIS.png" alt="" class="w-full h-full object-contain">
+                    <img src="/images/BAA/REV.DR.EFREKEE.EKPENYONG.png" alt="" class="w-full h-full object-cover" >
                 </div>
 
                 <div class="flex flex-col align-middle justify-center">
 
-                    <h1 class="font-bold">REV. DR. TAKIS E. CAIAFAS</h1>
+                    <h1 class="font-bold">REV. DR. EFREKE E. EKPENYONG</h1>
                     <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                    <h3>CROSS RIVER NIGERIA CONFERENCE</h3>
+                    <h3> NIGERIA CONFERENCE </h3>
 
                 </div>
             </div>
 
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center" id="SAMUEL_IBEMERE">
+              
 
-            <div class="flex flex-col-reverse md:flex-row gap-10 lg:gap-60 justify-center items-center" id="donald">
+              <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
+                  <img src="/images/BAA/REV.DR.SAMUELO.IBEMERE,SR.png" alt="" class="w-full h-full object-cover" >
+              </div>
+
                 <div class="flex flex-col align-middle justify-center">
 
-                <h1 class="font-bold">REV. DR. DONALD J. EKPO</h1>
-                <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                <h3>SOUTHERN NIGERIA CONFERENCE</h3>
+                  <h1 class="font-bold">REV. DR. SAMUEL O. IBEMERE, SR</h1>
+                  <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
+                  <h3>CENTRAL NIGERIA CONFERENCE 	 	 	</h3>
 
-                </div>
-
-                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
-                    <img src="/images/BAA/Dr-DONALD.png" alt="" class="w-full h-full object-cover" >
-                </div>
+              </div>
             </div>
 
 
-            <div class="flex flex-col md:flex-row gap-10 lg:gap-60 pb-8 justify-center items-center" id="OZIOHA">
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center" id="UMOROLU">
+                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
+                    <img src="/images/BAA/REV.DR.UMOROLU.png" alt="" class="w-full h-full object-cover" >
+                </div>
+
+                <div class="flex flex-col align-middle justify-center">
+
+                    <h1 class="font-bold">REV. DR. UMOR OLU</h1>
+                    <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
+                    <h3>  RIVERS  NIGERIA CONFERENCE  </h3>
+
+                </div>
+            </div>
+
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center " id="MichaelUdoh">
+                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
+                    <img src="/images/BAA/Rev.Dr.MichaelS.Udoh.jpg" alt="" class="w-full h-full object-cover" >
+                </div>
+
+                <div class="flex flex-col align-middle justify-center">
+
+                    <h1 class="font-bold">REV. DR. MICHAEL S. UDOH</h1>
+                    <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
+                    <h3>  LAGOS WEST NIGERIA CONFERENCE 	  </h3>
+
+                </div>
+            </div>
+
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center " id="SAMUEL_ASUAMIOM">
+                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
+                    <img src="/images/BAA/REV.DR.SAMUEL ASUAMIOM.png" alt="" class="w-full h-full object-cover" >
+                </div>
+
+                <div class="flex flex-col align-middle justify-center">
+
+                    <h1 class="font-bold">REV. DR. SAMUEL ASUAMIOM</h1>
+                    <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
+                    <h3> MAINLAND NIGERIA CONFERENCE 	 </h3>
+
+                </div>
+            </div>
+
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center" id="OZIOHA">
                 <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
                     <img src="/images/BAA/REV.BARR.OZIOHAV.EKEMAM.jpg" alt="" class="w-full h-full object-cover" >
                 </div>
@@ -55,132 +98,95 @@
             </div>
 
 
-            <div class="flex flex-col-reverse md:flex-row gap-10 lg:gap-60 justify-center items-center" id="CLEMENT">
-                <div class="flex flex-col align-middle justify-center">
 
-                <h1 class="font-bold">REV. CLEMENT  EGHUANOYE- GRAHAM</h1>
-                <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                <h3>PORT HARCOURT NIGERIA CONFERENCE 	</h3>
-
-                </div>
-
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center "  id="takis">
                 <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
-                    <img src="/images/BAA/REV.CLEMENT EGHUANOYE- GRAHAM.png" alt="" class="w-full h-full object-cover" >
-                </div>
-            </div>
-
-
-           <div class="flex flex-col md:flex-row gap-10 lg:gap-60 pb-8 justify-center items-center " id="EFREKEE">
-                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
-                    <img src="/images/BAA/REV.DR.EFREKEE.EKPENYONG.png" alt="" class="w-full h-full object-cover" >
+                    <img src="/images/BAA/Dr-TAKIS.png" alt="" class="w-full h-full object-contain">
                 </div>
 
                 <div class="flex flex-col align-middle justify-center">
 
-                    <h1 class="font-bold">REV. DR. EFREKE E. EKPENYONG</h1>
+                    <h1 class="font-bold">REV. DR. TAKIS E. CAIAFAS</h1>
                     <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                    <h3> NIGERIA CONFERENCE </h3>
+                    <h3>CROSS RIVER NIGERIA CONFERENCE</h3>
 
                 </div>
             </div>
 
-            <div class="flex flex-col-reverse md:flex-row gap-10 lg:gap-60 justify-center items-center" id="GODSON">
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center" id="GODSON">
+               
+
+               <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
+                   <img src="/images/BAA/REV.DR.GODSONN.ELIJAH.png" alt="" class="w-full h-full object-cover" >
+               </div> 
+               
+               <div class="flex flex-col align-middle justify-center">
+
+                   <h1 class="font-bold">REV. DR. GODSON N. ELIJAH</h1>
+                   <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
+                   <h3>SOUTHEASTERN NIGERIA CONFERENCE 	 	</h3>
+
+               </div>
+           </div>
+
+
+
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center" id="donald">
+                
+                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
+                    <img src="/images/BAA/Dr-DONALD.png" alt="" class="w-full h-full object-cover" >
+                </div>
+                
                 <div class="flex flex-col align-middle justify-center">
 
-                <h1 class="font-bold">REV. DR. GODSON N. ELIJAH</h1>
-                <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                <h3>SOUTHEASTERN NIGERIA CONFERENCE 	 	</h3>
-
-                </div>
-
-                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
-                    <img src="/images/BAA/REV.DR.GODSONN.ELIJAH.png" alt="" class="w-full h-full object-cover" >
-                </div>
-            </div>
-
-
-          <div class="flex flex-col md:flex-row gap-10 lg:gap-60 pb-8 justify-center items-center " id="SAMUEL_ASUAMIOM">
-                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
-                    <img src="/images/BAA/REV.DR.SAMUEL ASUAMIOM.png" alt="" class="w-full h-full object-cover" >
-                </div>
-
-                <div class="flex flex-col align-middle justify-center">
-
-                    <h1 class="font-bold">REV. DR. SAMUEL ASUAMIOM</h1>
+                    <h1 class="font-bold">REV. DR. DONALD J. EKPO</h1>
                     <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                    <h3> MAINLAND NIGERIA CONFERENCE 	 </h3>
+                    <h3>SOUTHERN NIGERIA CONFERENCE</h3>
 
                 </div>
+
+               
             </div>
 
-             <div class="flex flex-col-reverse md:flex-row gap-10 lg:gap-60 justify-center items-center" id="SAMUEL_IBEMERE">
-                <div class="flex flex-col align-middle justify-center">
 
-                <h1 class="font-bold">REV. DR. SAMUEL O. IBEMERE, SR</h1>
-                <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                <h3>CENTRAL NIGERIA CONFERENCE 	 	 	</h3>
-
-                </div>
-
-                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
-                    <img src="/images/BAA/REV.DR.SAMUELO.IBEMERE,SR.png" alt="" class="w-full h-full object-cover" >
-                </div>
-            </div>
-
-             <div class="flex flex-col md:flex-row gap-10 lg:gap-60 pb-8 justify-center items-center " id="UMOROLU">
-                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
-                    <img src="/images/BAA/REV.DR.UMOROLU.png" alt="" class="w-full h-full object-cover" >
-                </div>
-
-                <div class="flex flex-col align-middle justify-center">
-
-                    <h1 class="font-bold">REV. DR. UMOR OLU</h1>
-                    <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                    <h3>  RIVERS  NIGERIA CONFERENCE  </h3>
-
-                </div>
-            </div>
-
-            <div class="flex flex-col-reverse md:flex-row gap-10 lg:gap-60 justify-center items-center" id="UMOINYANG">
-                <div class="flex flex-col align-middle justify-center">
-
-                <h1 class="font-bold">REV. PROF. IMO E. UMOINYANG</h1>
-                <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                <h3>AKWA NIGERIA CONFERENCE 	</h3>
-
-                </div>
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center" id="UMOINYANG">
+                
 
                 <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
                     <img src="/images/BAA/REV.PROF.IMOE.UMOINYANG.png" alt="" class="w-full h-full object-cover" >
                 </div>
-            </div>
-
-
-            <div class="flex flex-col md:flex-row gap-10 lg:gap-60 pb-8 justify-center items-center " id="MichaelUdoh">
-                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
-                    <img src="/images/BAA/Rev.Dr.MichaelS.Udoh.jpg" alt="" class="w-full h-full object-cover" >
-                </div>
 
                 <div class="flex flex-col align-middle justify-center">
 
-                    <h1 class="font-bold">REV. DR. MICHAEL S. UDOH</h1>
+                    <h1 class="font-bold">REV. PROF. IMO E. UMOINYANG</h1>
                     <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
-                    <h3>  LAGOS WEST NIGERIA CONFERENCE 	  </h3>
+                    <h3>AKWA NIGERIA CONFERENCE 	</h3>
 
                 </div>
             </div>
 
+            
 
+            <div class="flex flex-col gap-12 pb-8 justify-center items-center" id="CLEMENT">
 
+                <div class="w-[250px] h-[250px] overflow-hidden rounded-full">
+                    <img src="/images/BAA/REV.CLEMENT EGHUANOYE- GRAHAM.png" alt="" class="w-full h-full object-cover" >
+                </div>  
+                <div class="flex flex-col align-middle justify-center">
 
+                    <h1 class="font-bold">REV. CLEMENT  EGHUANOYE- GRAHAM</h1>
+                    <h2>BISHOP’S ADMINISTRATIVE ASSISTANT</h2>
+                    <h3>PORT HARCOURT NIGERIA CONFERENCE 	</h3>
 
-           
+                </div>
+                
+            </div>
+               
 
         </div>
         </div>
 
-        <!-- Right Sidebar (Non-overlapping footer) -->
-            <aside
+            <!-- <aside
                 class="w-56 bg-gray-600 text-white hidden md:flex flex-col  py-6 shadow-lg transition-all duration-300 z-0 overflow-y-auto"
                 :class="isFixed ? 'fixed top-0 right-0 h-screen' : 'absolute top-0 right-0 h-full'"
                 >
@@ -205,9 +211,8 @@
 
 
                     
-                    <!-- <a href="#" class="hover:text-[#dec043]">Contact</a> -->
                 </nav>
-        </aside>
+        </aside> -->
     </div>
 
   </template>
