39f4a2579902e208989f7ca62d1d883ed697b192
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2025-05-27 22:07:24 +0100

    update

--- src/components/AboutusComponent/Missionimperatives.vue
+++ src/components/AboutusComponent/Missionimperatives.vue
@@ -12,7 +12,7 @@
     <div class="w-full flex flex-row flex-wrap gap-14 xl:px-72 mt-14 justify-between">
         <div v-for="(item, index) in mi" :key="index" class="w-96">
            <div>
-                <h3 class="font-bold">{{ item.tile }}</h3>
+                <h3 class="font-bold">{{ item.title }}</h3>
                 <p class="">{{ item.body }}</p>
            </div>
         </div>
@@ -24,47 +24,47 @@
 
 const mi = [
     {
-        tile : "1. No Poverty",
+        title : "1. No Poverty",
         body : "End poverty in all its forms everywhere"
     },
 
     {
-        tile : "2. Zero Hunger",
+        title : "2. Zero Hunger",
         body : "End hunger, achieve food security and improved nutrition and promote sustainable agriculture."
     },
 
     {
-        tile : "3. Good Health and Well-Being",
+        title : "3. Good Health and Well-Being",
         body : "Ensure healthy lives and promote well-being for all at all."
     },
 
     {
-        tile : "4. Quality Education",
+        title : "4. Quality Education",
         body : "Ensure inclusive and equitable quality education and promote lifelong learning opportunities."
     },
 
     {
-        tile : "5. Gender Equality",
+        title : "5. Gender Equality",
         body : "Achieve gender equality and empower all women and girls."
     },
 
     {
-        tile : "6. Clean Water and Sanitation",
+        title : "6. Clean Water and Sanitation",
         body : "Ensure availability and sustainable management of water and sanitation for all."
     },
 
     {
-        tile : "7. Affordable and Clean Energy",
+        title : "7. Affordable and Clean Energy",
         body : "Ensure access to affordable, reliable, sustainable and modern energy for all."
     },
 
     {
-        tile : "8. Decent Work and Economic Growth",
+        title : "8. Decent Work and Economic Growth",
         body : "Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all."
     },
 
     {
-        tile : "9. Industry, Innovation, and Infrastructure",
+        title : "9. Industry, Innovation, and Infrastructure",
         body : "Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation."
     },
     
--- src/views/Departments.vue
+++ src/views/Departments.vue
@@ -4,10 +4,70 @@
     </NavBar>
 
 
+    <div class="w-full gap-14 lg:px-72 mt-14 ">
+           <div v-for="(item, index) in dept" :key="index"  class="flex flex-col">
+                <h1 class="font-bold">{{ item.overall_tile }}</h1>
+                
+                <div class="flex flex-col lg:flex-row gap-14 justify-between mt-5">
+                    <div >
+                        {{ item.image }}
+                    </div>
+
+                    <div>
+                        <h3> <b>{{ item.body.leader }} </b></h3>
+                        <h3>  {{ item.body.office }}</h3>
+                        <h3>  {{ item.body.requirement }}</h3>
+
+                        <br>
+
+                        <h3>  {{ item.body.Description }}</h3>
+
+                        <br>
+
+                        <h3> <b>Motto</b>: {{ item.body.Motto }}</h3>
+                        <h3> <b>Colors</b>: {{ item.body.Colors }}</h3>
+                        <h3> <b>Theme Hymn</b>: {{ item.body.Theme_Hymn }}</h3>
+
+                        <br>
+
+                        <h3> <b>Mission</b>: {{ item.body.Mission }}</h3>
+
+                    </div>
+                </div>
+               
+           </div>
+    </div>
+
+
+
+
+
 </template>
 
 <script setup>
 import NavBar from '../components/NavBar.vue';
+
+
+
+const dept = [
+    {
+        overall_tile : "1. No Poverty",
+        image: "image",
+        body : {
+            leader : "leader",
+            office : "office",
+            requirement : "Parent Body Society (Age 41 & Up)",
+
+            Description: 'Description',
+
+            Motto : "Motto",
+            Colors: "Colors",
+            Theme_Hymn: "Theme_Hymn",
+            
+            Mission: "The Women's Home and Overseas Missionary Society includes the Parent Body, from age 41 to an unlimited age. The purpose of the Parent Body is to (1) promote growth in the knowledge and understanding of God and His plan of redemption for the world, as revealed through Jesus Christ and the power of the Holy Spirit; (2) teach the concepts of Christian missions and provide experiences for participation in mission work and its ministries; (3) exemplify the principles of Christian living and to win others to Christ; (4) Promote the cause of World Evangelism; and (5) serve as a financial support system to undergird the world mission outreach of The African Methodist Episcopal Zion Church, at home and overseas, to the end that, through the power of the Holy Spirit, Christ is exalted and God is glorified.",
+        },
+    },
+]
 
 
 </script>
