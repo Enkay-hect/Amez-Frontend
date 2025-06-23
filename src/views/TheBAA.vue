6f544e2413d88dbd147ee352c7dc74f050fb872b
Nkeneabasi Friday Aniekan <fridaynkeneabasi@gmail.com>
2025-06-23 12:03:39 +0100

    update

--- src/views/TheBAA.vue
+++ src/views/TheBAA.vue
@@ -13,64 +13,90 @@
                    <div class="card-body">
 
                        <h2 class="section-title" style="margin-top: 30px;"></h2>
-                       <div class="leadership-grid">
-                           <div class="leader-card">
-                               <img src="/images/BAAs/drifreke.png" alt="Rev. Dr. Ifreke E. Ekpenyong" class="leader-image">
-                               <h3>Rev. Dr. Ifreke E. Ekpenyong</h3>
-                               <div class="leader-position">Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/dribemere.png" alt="Rev. Dr. Samuel O. Ibemere, SR" class="leader-image">
-                               <h3>Rev. Dr. Samuel O. Ibemere, SR</h3>
-                               <div class="leader-position">Central Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/drumorlu.png" alt="Rev. Dr. Umor Olu" class="leader-image">
-                               <h3>Rev. Dr. Umor Olu</h3>
-                               <div class="leader-position">Rivers Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/drmike.jpeg" alt="Rev. Dr. Michael S. Udoh" class="leader-image">
-                               <h3>Rev. Dr. Michael S. Udoh</h3>
-                               <div class="leader-position">Lagos West Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/drasuamiom.png" alt="Rev. Dr. Samuel Asuamiom" class="leader-image">
-                               <h3>Rev. Dr. Samuel Asuamiom</h3>
-                               <div class="leader-position">Mainland Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/barrekemam.jpg" alt="Rev. Barr. Ozioha V. Ekemam" class="leader-image">
-                               <h3>Rev. Barr. Ozioha V. Ekemam</h3>
-                               <div class="leader-position">Northern Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/Drcaifas.png" alt="Rev. Dr. Takis E. Caiafas" class="leader-image">
-                               <h3>Rev. Dr. Takis E. Caiafas</h3>
-                               <div class="leader-position">Cross River Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/drndubisi.png" alt="Rev. Dr. Godson N. Elijah" class="leader-image">
-                               <h3>Rev. Dr. Godson N. Elijah</h3>
-                               <div class="leader-position">Southeastern Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/Drekpo.png" alt="Rev. Dr. Donald J. Ekpo" class="leader-image">
-                               <h3>Rev. Dr. Donald J. Ekpo</h3>
-                               <div class="leader-position">Southern Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/profumoinyang.png" alt="Rev.Prof. Imo E. Umoinyang" class="leader-image">
-                               <h3>Rev.Prof. Imo E. Umoinyang</h3>
-                               <div class="leader-position">Akwa Nigeria Conference</div>
-                           </div>
-                           <div class="leader-card">
-                               <img src="/images/BAAs/drclement.png" alt="Rev. Clement Eghuanoye-Graham" class="leader-image">
-                               <h3>Rev. Clement Eghuanoye-Graham</h3>
-                               <div class="leader-position">Port Harcourt Nigeria Conference</div>
-                           </div>
+                          <div class="leadership-grid">
+                              <router-link to="/Annual-Conference/Nigeria-Conference">
+                                  <div class="leader-card">
+                                    <img src="/images/BAAs/drifreke.png" alt="Rev. Dr. Ifreke E. Ekpenyong" class="leader-image">
+                                    <h3>Rev. Dr. Ifreke E. Ekpenyong</h3>
+                                    <div class="leader-position">Nigeria Conference</div>
+                                </div>
+                              </router-link>
 
-                       </div>
+                             <router-link to="/Annual-Conference/Central-Nigeria-Conference">
+                              <div class="leader-card">
+                                  <img src="/images/BAAs/dribemere.png" alt="Rev. Dr. Samuel O. Ibemere, SR" class="leader-image">
+                                  <h3>Rev. Dr. Samuel O. Ibemere, SR</h3>
+                                  <div class="leader-position">Central Nigeria Conference</div>
+                              </div>
+                             </router-link>
+
+                              <router-link to="/Annual-Conference/Rivers-Nigeria-Conference">
+                                <div class="leader-card">
+                                    <img src="/images/BAAs/drumorlu.png" alt="Rev. Dr. Umor Olu" class="leader-image">
+                                    <h3>Rev. Dr. Umor Olu</h3>
+                                    <div class="leader-position">Rivers Nigeria Conference</div>
+                                </div>
+                              </router-link>
+
+                             <router-link to="/Annual-Conference/LagosWestNigeria-Conference">
+                              <div class="leader-card">
+                                  <img src="/images/BAAs/drmike.jpeg" alt="Rev. Dr. Michael S. Udoh" class="leader-image">
+                                  <h3>Rev. Dr. Michael S. Udoh</h3>
+                                  <div class="leader-position">Lagos West Nigeria Conference</div>
+                              </div>
+                             </router-link>
+
+                              <router-link to="/Annual-Conference/Mainland-Nigeria-Conference">
+                                <div class="leader-card">
+                                  <img src="/images/BAAs/drasuamiom.png" alt="Rev. Dr. Samuel Asuamiom" class="leader-image">
+                                  <h3>Rev. Dr. Samuel Asuamiom</h3>
+                                  <div class="leader-position">Mainland Nigeria Conference</div>
+                                </div>
+                              </router-link>
+
+                              <router-link to="/Annual-Conference/Northern-Nigeria-Conference">
+                                <div class="leader-card">
+                                  <img src="/images/BAAs/barrekemam.jpg" alt="Rev. Barr. Ozioha V. Ekemam" class="leader-image">
+                                  <h3>Rev. Barr. Ozioha V. Ekemam</h3>
+                                  <div class="leader-position">Northern Nigeria Conference</div>
+                                </div>
+                              </router-link>
+
+                             <router-link to="/Annual-Conference/CrossRiver-Conference">
+                                <div class="leader-card">
+                                    <img src="/images/BAAs/Drcaifas.png" alt="Rev. Dr. Takis E. Caiafas" class="leader-image">
+                                    <h3>Rev. Dr. Takis E. Caiafas</h3>
+                                    <div class="leader-position">Cross River Nigeria Conference</div>
+                                </div>
+                             </router-link>
+
+                              <router-link to="/Annual-Conference/SouthEastern-Conference">
+                                <div class="leader-card">
+                                  <img src="/images/BAAs/drndubisi.png" alt="Rev. Dr. Godson N. Elijah" class="leader-image">
+                                  <h3>Rev. Dr. Godson N. Elijah</h3>
+                                  <div class="leader-position">Southeastern Nigeria Conference</div>
+                              </div>
+                              </router-link>
+
+                              <div class="leader-card">
+                                  <img src="/images/BAAs/Drekpo.png" alt="Rev. Dr. Donald J. Ekpo" class="leader-image">
+                                  <h3>Rev. Dr. Donald J. Ekpo</h3>
+                                  <div class="leader-position">Southern Nigeria Conference</div>
+                              </div>
+
+                              <div class="leader-card">
+                                  <img src="/images/BAAs/profumoinyang.png" alt="Rev.Prof. Imo E. Umoinyang" class="leader-image">
+                                  <h3>Rev.Prof. Imo E. Umoinyang</h3>
+                                  <div class="leader-position">Akwa Nigeria Conference</div>
+                              </div>
+
+                              <div class="leader-card">
+                                  <img src="/images/BAAs/drclement.png" alt="Rev. Clement Eghuanoye-Graham" class="leader-image">
+                                  <h3>Rev. Clement Eghuanoye-Graham</h3>
+                                  <div class="leader-position">Port Harcourt Nigeria Conference</div>
+                              </div>
+
+                          </div>
                    </div>
                </div>
            </div>
