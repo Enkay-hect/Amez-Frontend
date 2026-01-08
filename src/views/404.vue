eb39d115cf7127f0c03553e647fb555ff65e7806
Aniefiok Asuquo <aniefiokasuquo33@gmail.com>
2026-01-08 18:03:53 +0100

    testing aubernard

--- src/views/404.vue
+++ src/views/404.vue
@@ -17,6 +17,8 @@
           class="mt-6 inline-block px-6 py-3 bg-blue-600 text-white text-lg rounded-lg shadow-md hover:bg-blue-700 transition-all"
         >
           Go Back Home
+
+          
         </router-link>
       </div>
     </div>
