ded5d3972fdd06c2f41c138aaea7f750b80b6d79
Aniefiok Asuquo <aniefiokasuquo33@gmail.com>
2026-05-17 10:05:15 +0100

    update

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -68,7 +68,7 @@
           </div> -->
 
           <!-- Card 3 -->
-          <div class="stream-card">
+          <!-- <div class="stream-card">
             <div class="thumbnail" style="background-image: url('https://images.unsplash.com/photo-1511632765486-a01980e01a18?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80');">
               <div class="play-icon"><i class="fas fa-play"></i></div>
             </div>
@@ -78,7 +78,7 @@
               <p>Theme: <strong>Leadership and Legacy</strong></p>
               <a href="#" class="btn-outline">Set Reminder</a>
             </div>
-          </div>
+          </div> -->
 
           <!-- Card 4 -->
           <div class="stream-card">
