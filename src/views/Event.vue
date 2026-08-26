70ab98a5277da6b7a074c7f2b52439a742eb69da
Aniefiok Asuquo <aniefiokasuquo33@gmail.com>
2026-08-26 15:04:35 +0100

    update

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -23,7 +23,7 @@
         <div class="live-now"><i class="fas fa-circle"></i> LIVE NOW</div>
         <div class="video-wrapper">
           <!-- Replace with your actual YouTube embed or live stream -->
-          <iframe src="https://www.youtube.com/embed/3nAxmbFnYac?autoplay=1&mute=1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/5yQ1faT2y80?autoplay=1&mute=1&playsinline=1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </div>
 
         <div class="video-info">
